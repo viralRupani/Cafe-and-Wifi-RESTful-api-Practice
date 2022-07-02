a88c3b2ac5e4e55f6fdf2d0b19a49ac149d6c6fb
viral rupani <viralrupani12017@gmail.com>
2022-07-02 21:47:02 +0530

    database path update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:////professional/Cafe and Wifi/cafes.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
